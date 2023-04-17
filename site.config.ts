e6737b8b9d0e59cc8c1c8d6edddeb202913bc9eb
0228XinSc <74149305+0228XinSc@users.noreply.github.com>
2023-04-17 22:23:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '4320ef57e1cc443cab378f9f61378255',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Xinsc_blog',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Xin_sc',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '真正的英雄不是改变世界，而改变自己生活的每一天',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
